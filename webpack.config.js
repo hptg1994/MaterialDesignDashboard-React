c7691cfda83e937f5dc5db240fec23a892f3b93c
hptg1994 <hptg1994@gmail.com>
2018-04-16 17:20:45 -0400

    Done,testing now

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path')
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
+}
 
 module.exports = (env) => {
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css')
   return {
-    entry: ['babel-polyfill','./src/index.js'],
+    entry: './src/index.js',
     output: {
       path: path.join(__dirname,'public','dist'),
       filename: 'bundle.js'
@@ -20,12 +29,25 @@ module.exports = (env) => {
         exclude:/node_modules/
       },{
         test:/\.s?css$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
+        use:CSSExtract.extract({
+          use:[{
+            loader:'css-loader',
+            options:{
+              sourceMap: true
+            }
+          },{
+            loader:'sass-loader',
+            options:{
+              sourceMap: true
+            }
+          }]
+        })
       },{
         test:/\.svg$/,
         use: 'svg-loader'
       }]
     },
+    plugins: [CSSExtract],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname,'public'),
